7c1b06a31f9003f27fd0677580488f39753f5a0d
Gustav Zickert <zickert@kth.se>
2017-06-14 08:19:03 +0200

    WIP: Fix more things according to comments on PR

--- odl/fom/fom_example.py
+++ odl/fom/fom_example.py
@@ -1,4 +1,4 @@
-"""Simple test of implemented figure-of-merits (FOMs)
+"""Simple example of implemented figure-of-merits (FOMs)
 
 Numerical test of a few implemented FOMs (mean square error, mean
 absolute error, blur, and false structures) as a function of
@@ -8,23 +8,12 @@ increasing noise level.
 
 import odl
 import numpy as np
-# import fom
 import matplotlib.pyplot as plt
 
 # Discrete reconstruction space: discretized functions on the rectangle
-# [-20, 20]^2 with 300 samples per dimension.
+# [-20, 20]^2 with 100 samples per dimension.
 reco_space = odl.uniform_discr(
     min_pt=[-20, -20], max_pt=[20, 20], shape=[100, 100])
-
-# Make a parallel beam geometry with flat detector
-# Angles: uniformly spaced, n = 360, min = 0, max = pi
-angle_partition = odl.uniform_partition(0, np.pi, 360)
-# Detector: uniformly sampled, n = 558, min = -30, max = 30
-detector_partition = odl.uniform_partition(-30, 30, 558)
-geometry = odl.tomo.Parallel2dGeometry(angle_partition, detector_partition)
-
-# Ray transform (= forward projection). We use the 'skimage' backend.
-ray_trafo = odl.tomo.RayTransform(reco_space, geometry)
 
 # Create a discrete Shepp-Logan phantom (modified version)
 phantom = odl.phantom.shepp_logan(reco_space, modified=True)
